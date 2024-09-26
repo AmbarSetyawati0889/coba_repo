aae26df59538ddd84e1bb9dea7d65721ddfe11b2
AmbarSetyawati0889 <ambarsetyawati0889@gmail.com>
2024-09-26 14:21:43 +0700

    Crud data pelanggan admin lte

--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -1,6 +1,33 @@
-@extends('layout.template')
+@extends('layout.admin')
         <!-- START DATA -->
-@section ('konten')
+@section ('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Data Pelanggan</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v2</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Pelanggan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body class="bg-light">
+    <main class="container">
         <div class="my-3 p-3 bg-body rounded shadow-sm">
                 <!-- FORM PENCARIAN -->
                 <div class="pb-3">
@@ -18,12 +45,12 @@
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th class="col-md-1">ID</th>
-                            <th class="col-md-3">Nama</th>
-                            <th class="col-md-4">Email</th>
-                            <th class="col-md-2">No.Telp</th>
-                            <th class="col-md-2">Alamat</th>
-                            <th class="col-md-2">Aksi</th>
+                            <th class="col-md-1"style="color: black;">ID</th>
+                            <th class="col-md-3"style="color: black;">Nama</th>
+                            <th class="col-md-4"style="color: black;">Email</th>
+                            <th class="col-md-2"style="color: black;">No.Telp</th>
+                            <th class="col-md-2"style="color: black;">Alamat</th>
+                            <th class="col-md-2"style="color: black;">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,13 +61,34 @@
                             <td>089609799182</td>
                             <td>Sucen Triharjo Sleman</td>
                             <td>
-                                <a href='' class="btn btn-warning btn-sm">Edit</a>
-                                <a href='' class="btn btn-danger btn-sm">Del</a>
+                                 <div style="display: flex; gap: 5px;">
+                                     <a href="#" class="btn btn-warning btn-sm">Edit</a>
+                                     <a href="#" class="btn btn-danger btn-sm">Del</a>
+                                 </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="color: black;">002</td>
+                            <td>Nurul Hidayah</td>
+                            <td>nurul@gmail.com</td>
+                            <td>089609799992</td>
+                            <td>Sucen Triharjo Sleman</td>
+                            <td>
+                                 <div style="display: flex; gap: 5px;">
+                                     <a href="#" class="btn btn-warning btn-sm">Edit</a>
+                                     <a href="#" class="btn btn-danger btn-sm">Del</a>
+                                 </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
           </div>
+          <!-- AKHIR DATA -->
+     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
+</div>
 @endsection
 
      
